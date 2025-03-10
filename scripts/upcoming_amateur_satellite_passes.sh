55327f236c30b0bda02071e3f866281c613ce051
FrontierSat team <general@calgarytospace.ca>
2025-03-10 14:25:20 -0600

    Show radio info

--- scripts/upcoming_amateur_satellite_passes.sh
+++ scripts/upcoming_amateur_satellite_passes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 (cd ${HOME}/src/simple_sat_ops
-next_in_queue satellites/amateur.tle 0 1000 --list --reverse 
+next_in_queue satellites/amateur.tle 0 1000 --list --reverse --show-radio-info
 )
