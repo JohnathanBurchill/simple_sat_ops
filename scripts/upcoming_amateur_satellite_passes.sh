dfd8fffe81407b87a1d0915520f4ac70b0100814
FrontierSat team <general@calgarytospace.ca>
2025-03-13 09:25:29 -0600

    Do not pass fourth arg if it is empty

--- scripts/upcoming_amateur_satellite_passes.sh
+++ scripts/upcoming_amateur_satellite_passes.sh
@@ -3,5 +3,9 @@
 satellite="$1"
 
 (cd ${HOME}/src/simple_sat_ops
-next_in_queue satellites/amateur.tle 0 1000 "${satellite}" --list --reverse --show-radio-info
+if [[ -n "${satellite}" ]]; then
+    next_in_queue satellites/amateur.tle 0 1000 "${satellite}" --list --reverse --show-radio-info
+else
+    next_in_queue satellites/amateur.tle 0 1000 --list --reverse --show-radio-info
+fi
 )
