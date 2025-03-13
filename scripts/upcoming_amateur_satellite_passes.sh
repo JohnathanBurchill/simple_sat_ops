c50ecd55df9b1b55c785e06af53376b7363c55d8
FrontierSat team <general@calgarytospace.ca>
2025-03-13 09:19:21 -0600

    Optional satellite name to find upcoming passes for

--- scripts/upcoming_amateur_satellite_passes.sh
+++ scripts/upcoming_amateur_satellite_passes.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+satellite="$1"
+
 (cd ${HOME}/src/simple_sat_ops
-next_in_queue satellites/amateur.tle 0 1000 --list --reverse --show-radio-info
+next_in_queue satellites/amateur.tle 0 1000 "${satellite}" --list --reverse --show-radio-info
 )
