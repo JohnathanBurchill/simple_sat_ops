933d5f48890212c1b9c51a00710dcec83a6540e1
FrontierSat team <general@calgarytospace.ca>
2025-05-04 06:52:10 -0600

    Boilerplate telemetry framework

---
+++ telemetry.h
@@ -0,0 +1,20 @@
+#ifndef TELEMETRY_H
+#define TELEMETRY_H
+
+enum TELEMETRY_RETURN_CODES {
+    TELEMETRY_OK = 0,
+};
+
+typedef struct telemetry {
+    int decoding;
+    int encoding;
+} telemetry_t;
+
+
+int init_telemetry_decoder(telemetry_t *state);
+int telemetry_decoder_cleanup(telemetry_t *state);
+int decode_live_audio_to_telemetry(telemetry_t *state);
+int decode_file_audio_to_telemetry(telemetry_t *state, char *filename);
+int encode_telecommand_bytes_to_audio(telemetry_t *state);
+
+#endif // TELEMETRY_H
